3123db0efe2b3d5fc1545b00446012ac6a7f8b50
Loris Leiva <loris.leiva@gmail.com>
2021-08-29 21:45:54 +0100

    wip

--- tests/MustacheTest.php
+++ tests/MustacheTest.php
@@ -1,1 +1,44 @@
 <?php
+
+use Lorisleiva\Vary\Vary;
+
+it('can replace all instances of a mustache variable with the given value', function () {
+    $content = <<<END
+        Perfection of character: to live your last {{ unit }}, every {{ unit }},
+        without frenzy, or sloth, or pretense.
+    END;
+
+    $variant = Vary::string($content)->mustache('unit', 'day');
+
+    expect($variant->toString())->toBe(<<<END
+        Perfection of character: to live your last day, every day,
+        without frenzy, or sloth, or pretense.
+    END);
+});
+
+it('can replace multiple mustache variables within a given array of data', function () {
+    $content = <<<END
+        Perfection of {{ entity }}: to live your last {{ unit }}, every {{ unit }},
+        without frenzy, {{ gate }} sloth, {{ gate }} pretense.
+    END;
+
+    $variant = Vary::string($content)->mustacheAll([
+        'entity' => 'self',
+        'unit' => 'minute',
+        'gate' => 'nor',
+    ]);
+
+    expect($variant->toString())->toBe(<<<END
+        Perfection of self: to live your last minute, every minute,
+        without frenzy, nor sloth, nor pretense.
+    END);
+});
+
+it('can replace mustache variable no matter how many whitespace is provided', function () {
+    expect(Vary::string('{{unit}}')->mustache('unit', 'day')->toString())->toBe('day');
+    expect(Vary::string('{{ unit}}')->mustache('unit', 'day')->toString())->toBe('day');
+    expect(Vary::string('{{unit }}')->mustache('unit', 'day')->toString())->toBe('day');
+    expect(Vary::string('{{ unit }}')->mustache('unit', 'day')->toString())->toBe('day');
+    expect(Vary::string('{{      unit      }}')->mustache('unit', 'day')->toString())->toBe('day');
+    expect(Vary::string("{{ unit \t\n\r}}")->mustache('unit', 'day')->toString())->toBe('day');
+});
