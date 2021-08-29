eb765e606e5912c9f1d2997fa454b17284aba91d
Loris Leiva <loris.leiva@gmail.com>
2021-08-29 21:20:13 +0100

    wip

--- tests/ReplaceTest.php
+++ tests/ReplaceTest.php
@@ -1,1 +1,16 @@
 <?php
+
+use Lorisleiva\Vary\Vary;
+
+it('can replace all instances of one text with another', function () {
+
+    $content = <<<END
+        Perfection of character: to live your last day, every day,
+        without frenzy, or sloth, or pretense.
+    END;
+
+    expect(Vary::string($content)->replace('day', 'week')->toString())->toBe(<<<END
+        Perfection of character: to live your last week, every week,
+        without frenzy, or sloth, or pretense.
+    END);
+});
