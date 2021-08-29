e5ab51e8083c882a3f17d8cb75c40ab1eb620a2d
lorisleiva <lorisleiva@users.noreply.github.com>
2021-08-29 20:20:37 +0000

    Fix styling

--- tests/ReplaceTest.php
+++ tests/ReplaceTest.php
@@ -3,7 +3,6 @@
 use Lorisleiva\Vary\Vary;
 
 it('can replace all instances of one text with another', function () {
-
     $content = <<<END
         Perfection of character: to live your last day, every day,
         without frenzy, or sloth, or pretense.
