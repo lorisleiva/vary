1d28d97056a56a22e9ed2a735bfa2ac5e03314e4
Loris Leiva <loris.leiva@gmail.com>
2021-09-02 13:00:34 +0100

    wip

--- src/Concerns/ProvidesAccessors.php
+++ src/Concerns/ProvidesAccessors.php
@@ -2,6 +2,8 @@
 
 namespace Lorisleiva\Vary\Concerns;
 
+use Illuminate\Support\Str;
+
 trait ProvidesAccessors
 {
     public function getPath(): ?string
@@ -12,5 +14,15 @@ trait ProvidesAccessors
     public function toString(): string
     {
         return $this->value;
+    }
+
+    public function match(string $pattern): string
+    {
+        return Str::match($pattern, $this->value);
+    }
+
+    public function matchAll(string $pattern): array
+    {
+        return Str::matchAll($pattern, $this->value)->toArray();
     }
 }
--- tests/ProvidesAccessorsTest.php
+++ tests/ProvidesAccessorsTest.php
@@ -11,3 +11,17 @@ it('returns the entire content as a string', function () {
     expect(Vary::string('Some text')->toString())
         ->toBe('Some text');
 });
+
+it('returns the text matching a given pattern', function () {
+    $variant = Vary::string('One apple pie. One humble pie. One apple TV.');
+
+    expect($variant->match('/One .*? pie/'))->toBe('One apple pie');
+    expect($variant->match('/One (.*?) pie/'))->toBe('apple');
+});
+
+it('returns an array of all texts matching a given pattern', function () {
+    $variant = Vary::string('One apple pie. One humble pie. One apple TV.');
+
+    expect($variant->matchAll('/One .*? pie/'))->toBe(['One apple pie', 'One humble pie']);
+    expect($variant->matchAll('/One (.*?) pie/'))->toBe(['apple', 'humble']);
+});
