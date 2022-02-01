4b4a7e7480a131ee4aa345e6fe59c5f50e3d9ef6
Loris Leiva <loris.leiva@gmail.com>
2022-02-01 17:38:51 +0000

    wip

--- tests/AltersMustachesTest.php
+++ tests/AltersMustachesTest.php
@@ -2,23 +2,21 @@
 
 use Lorisleiva\Vary\Vary;
 
-it('replaces all instances of a mustache variable with the given value', function () {
+test('replaceMustache', function () {
     Vary::string('One apple {{ patisserie }}. One humble {{ patisserie }}. One apple TV.')
         ->replaceMustache('patisserie', 'pie')
         ->tap(expectVariantToBe('One apple pie. One humble pie. One apple TV.'));
-});
 
-it('replaces multiple mustache variables within a given array of data', function () {
-    Vary::string('{{ number }} apple {{ patisserie }}. {{ number }} humble {{ patisserie }}. {{ number }} apple TV.')
-        ->replaceAllMustaches(['number' => 'Two', 'patisserie' => 'pie'])
-        ->tap(expectVariantToBe('Two apple pie. Two humble pie. Two apple TV.'));
-});
-
-it('ignores any whitespace within the mustache variables', function () {
     expect(Vary::string('{{number}}')->replaceMustache('number', 'One')->toString())->toBe('One');
     expect(Vary::string('{{ number}}')->replaceMustache('number', 'One')->toString())->toBe('One');
     expect(Vary::string('{{number }}')->replaceMustache('number', 'One')->toString())->toBe('One');
     expect(Vary::string('{{ number }}')->replaceMustache('number', 'One')->toString())->toBe('One');
     expect(Vary::string('{{      number      }}')->replaceMustache('number', 'One')->toString())->toBe('One');
     expect(Vary::string("{{ number \t\n\r}}")->replaceMustache('number', 'One')->toString())->toBe('One');
+});
+
+test('replaceAllMustaches', function () {
+    Vary::string('{{ number }} apple {{ patisserie }}. {{ number }} humble {{ patisserie }}. {{ number }} apple TV.')
+        ->replaceAllMustaches(['number' => 'Two', 'patisserie' => 'pie'])
+        ->tap(expectVariantToBe('Two apple pie. Two humble pie. Two apple TV.'));
 });
--- tests/AltersWhitespaceTest.php
+++ tests/AltersWhitespaceTest.php
@@ -2,39 +2,39 @@
 
 use Lorisleiva\Vary\Vary;
 
-it('selects a fragment before any whitespace', function () {
-    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
-        ->selectBeforeWhitespace(expectVariantToBe("  \t\n  \r\n Hello Moto"));
-});
-
-it('selects a fragment after any whitespace', function () {
-    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
-        ->selectAfterWhitespace(expectVariantToBe("Hello Moto \n  \t"));
-});
-
-it('selects a fragment between any whitespace', function () {
-    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
-        ->selectBetweenWhitespace(expectVariantToBe("Hello Moto"));
-});
-
-it('prepends some text after any whitespace', function () {
-    Vary::string(" \t\n Hello World")
-        ->prependAfterWhitespace('// ')
-        ->tap(expectVariantToBe(" \t\n // Hello World"));
-});
-
-it('appends some text before any whitespace', function () {
+test('appendBeforeWhitespace', function () {
     Vary::string("Hello World \t\n ")
         ->appendBeforeWhitespace(';')
         ->tap(expectVariantToBe("Hello World; \t\n "));
 });
 
-it('returns any whitespace at the left of the content', function () {
+test('getLeftWhitespace', function () {
     expect(Vary::string(" \t\n \r Hello World \t\n ")->getLeftWhitespace())->toBe(" \t\n \r ");
     expect(Vary::string("Hello World")->getLeftWhitespace())->toBe('');
 });
 
-it('returns any whitespace at the right of the content', function () {
+test('getRightWhitespace', function () {
     expect(Vary::string(" \t\n \r Hello World \t\n ")->getRightWhitespace())->toBe(" \t\n ");
     expect(Vary::string("Hello World")->getRightWhitespace())->toBe('');
+});
+
+test('prependAfterWhitespace', function () {
+    Vary::string(" \t\n Hello World")
+        ->prependAfterWhitespace('// ')
+        ->tap(expectVariantToBe(" \t\n // Hello World"));
+});
+
+test('selectAfterWhitespace', function () {
+    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
+        ->selectAfterWhitespace(expectVariantToBe("Hello Moto \n  \t"));
+});
+
+test('selectBeforeWhitespace', function () {
+    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
+        ->selectBeforeWhitespace(expectVariantToBe("  \t\n  \r\n Hello Moto"));
+});
+
+test('selectBetweenWhitespace', function () {
+    Vary::string("  \t\n  \r\n Hello Moto \n  \t")
+        ->selectBetweenWhitespace(expectVariantToBe("Hello Moto"));
 });
