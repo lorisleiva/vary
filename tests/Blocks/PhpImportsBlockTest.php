de3afc799dcb437540332251381095c29b717a50
Loris Leiva <loris.leiva@gmail.com>
2022-02-03 10:39:07 +0000

    wip

--- tests/Blocks/PhpImportsBlockTest.php
+++ tests/Blocks/PhpImportsBlockTest.php
@@ -4,6 +4,157 @@ use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use Lorisleiva\Vary\Vary;
 
+test('add', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+
+        use App\Models\Article;
+        use App\Models\User;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->add(Arr::class, Str::class)
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use Illuminate\Support\Str;
+            use App\Http\Controllers\FetchArticleController;
+            use Illuminate\Support\Arr;
+            use Illuminate\Support\Str;
+
+            use App\Models\Article;
+            use App\Models\User;
+            PHP
+        ));
+
+    Vary::string("use Illuminate\Support\Str;")
+        ->phpImports()->add(Arr::class)
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use Illuminate\Support\Str;
+            use Illuminate\Support\Arr;
+            PHP
+        ));
+});
+
+test('delete', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+
+        use App\Models\Article;
+        use Illuminate\Support\Str;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->delete(Str::class, 'App\Models\Article')
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use App\Http\Controllers\FetchArticleController;
+
+            PHP
+        ));
+
+    Vary::string($useStatements)
+        ->phpImports()->delete()
+        ->tap(expectVariantToBe(''));
+});
+
+test('replace', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+
+        use App\Models\Article;
+        use Illuminate\Support\Str;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->replace(Str::class, Arr::class)
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use Illuminate\Support\Arr;
+            use App\Http\Controllers\FetchArticleController;
+
+            use App\Models\Article;
+            use Illuminate\Support\Arr;
+            PHP
+        ));
+});
+
+test('replaceAll', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+
+        use App\Models\Article;
+        use Illuminate\Support\Str;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->replaceAll([
+            Str::class => Arr::class,
+            'App\Models\Article' => 'App\Models\Post',
+        ])
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use Illuminate\Support\Arr;
+            use App\Http\Controllers\FetchArticleController;
+
+            use App\Models\Post;
+            use Illuminate\Support\Arr;
+            PHP
+        ));
+});
+
+test('sort', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+        use Illuminate\Support\Facades\Route;
+
+        use App\Models\User;
+        use App\Models\Article;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->sort()
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use App\Http\Controllers\FetchArticleController;
+            use Illuminate\Support\Facades\Route;
+            use Illuminate\Support\Str;
+
+            use App\Models\Article;
+            use App\Models\User;
+            PHP
+        ));
+});
+
+test('sortByLength', function () {
+    $useStatements = <<<PHP
+        use Illuminate\Support\Str;
+        use App\Http\Controllers\FetchArticleController;
+        use Illuminate\Support\Facades\Route;
+
+        use App\Models\Article;
+        use App\Models\User;
+        PHP;
+
+    Vary::string($useStatements)
+        ->phpImports()->sortByLength()
+        ->tap(expectVariantToBe(
+            <<<PHP
+            use Illuminate\Support\Str;
+            use Illuminate\Support\Facades\Route;
+            use App\Http\Controllers\FetchArticleController;
+
+            use App\Models\User;
+            use App\Models\Article;
+            PHP
+        ));
+});
+
 it('selects PHP import statements', function () {
     $useStatements = <<<PHP
         // Before
@@ -77,151 +228,6 @@ it('does not select trait imports as PHP import statements', function () {
             class Foo {
                 use SomeTrait;
             }
-            PHP
-        ));
-});
-
-it('sorts PHP import statements', function () {
-    $useStatements = <<<PHP
-        use Illuminate\Support\Str;
-        use App\Http\Controllers\FetchArticleController;
-        use Illuminate\Support\Facades\Route;
-
-        use App\Models\User;
-        use App\Models\Article;
-        PHP;
-
-    Vary::string($useStatements)
-        ->phpImports()->sort()
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use App\Http\Controllers\FetchArticleController;
-            use Illuminate\Support\Facades\Route;
-            use Illuminate\Support\Str;
-
-            use App\Models\Article;
-            use App\Models\User;
-            PHP
-        ));
-});
-
-it('sorts PHP import statements by length', function () {
-    $useStatements = <<<PHP
-        use Illuminate\Support\Str;
-        use App\Http\Controllers\FetchArticleController;
-        use Illuminate\Support\Facades\Route;
-
-        use App\Models\Article;
-        use App\Models\User;
-        PHP;
-
-    Vary::string($useStatements)
-        ->phpImports()->sortByLength()
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use Illuminate\Support\Str;
-            use Illuminate\Support\Facades\Route;
-            use App\Http\Controllers\FetchArticleController;
-
-            use App\Models\User;
-            use App\Models\Article;
-            PHP
-        ));
-});
-
-it('adds PHP imports', function () {
-    $useStatements = <<<PHP
-        use Illuminate\Support\Str;
-        use App\Http\Controllers\FetchArticleController;
-
-        use App\Models\Article;
-        use App\Models\User;
-        PHP;
-
-    Vary::string($useStatements)
-        ->phpImports()->add(Arr::class, Str::class)
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use Illuminate\Support\Str;
-            use App\Http\Controllers\FetchArticleController;
-            use Illuminate\Support\Arr;
-            use Illuminate\Support\Str;
-
-            use App\Models\Article;
-            use App\Models\User;
-            PHP
-        ));
-
-    Vary::string("use Illuminate\Support\Str;")
-        ->phpImports()->add(Arr::class)
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use Illuminate\Support\Str;
-            use Illuminate\Support\Arr;
-            PHP
-        ));
-});
-
-it('replaces PHP imports', function () {
-    $useStatements = <<<PHP
-        use Illuminate\Support\Str;
-        use App\Http\Controllers\FetchArticleController;
-
-        use App\Models\Article;
-        use Illuminate\Support\Str;
-        PHP;
-
-    Vary::string($useStatements)
-        ->phpImports()->replace(Str::class, Arr::class)
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use Illuminate\Support\Arr;
-            use App\Http\Controllers\FetchArticleController;
-
-            use App\Models\Article;
-            use Illuminate\Support\Arr;
-            PHP
-        ));
-
-    Vary::string($useStatements)
-        ->phpImports()->replaceAll([
-            Str::class => Arr::class,
-            'App\Models\Article' => 'App\Models\Post',
-        ])
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use Illuminate\Support\Arr;
-            use App\Http\Controllers\FetchArticleController;
-
-            use App\Models\Post;
-            use Illuminate\Support\Arr;
-            PHP
-        ));
-});
-
-it('deletes PHP imports', function () {
-    $useStatements = <<<PHP
-        use Illuminate\Support\Str;
-        use App\Http\Controllers\FetchArticleController;
-
-        use App\Models\Article;
-        use Illuminate\Support\Str;
-        PHP;
-
-    Vary::string($useStatements)
-        ->phpImports()->delete(Str::class, 'App\Models\Article')
-        ->tap(expectVariantToBe(
-            <<<PHP
-            use App\Http\Controllers\FetchArticleController;
-
-            PHP
-        ));
-
-    Vary::string($useStatements)
-        ->phpImports()->delete()
-        ->tap(expectVariantToBe(
-            <<<PHP
-
             PHP
         ));
 });
